c69eef4a16ee344c00b18fa137122968de32bbc3
raphaelbsr <raphael@datac.com.br>
2020-02-22 10:53:45 -0300

    fix REACT_APP_MIO_ATUH_TOKEN

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,7 +6,7 @@ const jwtDecode = require("jwt-decode");
  * Recomendo seguir o padrão persist:@mio-template:token
  * alterando a palavra template para o nome da aplicação
  */
-export const TOKEN_KEY = process.env.REACT_APP_MIO_ATUH_TOKEN;
+export const TOKEN_KEY = process.env.REACT_APP_MIO_AUTH_TOKEN;
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = token => {
