f4344df609eefef7e251884f715b92eafbf66ac7
raphaelbsr <raphael@datac.com.br>
2020-02-22 10:46:21 -0300

    export login function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import Autenticacao from "./Autenticacao";
 import { isAuthenticated, getToken, getData, logout } from "./services/auth";
-export { Autenticacao, isAuthenticated, getToken, getData, logout };
+export { Autenticacao, isAuthenticated, getToken, getData, login };
